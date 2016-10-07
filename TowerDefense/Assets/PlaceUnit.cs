0d19ee3264500f8ba7b3a16ff1da60eda3928f28
Alicia Morales Carrasco <amoralca16@alumnes.ub.edu>
2016-10-07 20:10:22 +0200

    - Add all.

---
+++ TowerDefense/Assets/PlaceUnit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceUnit : MonoBehaviour {
+
+
+    void OnMouseUp()
+    {
+        Debug.Log("TowerSpot clicked.");
+        ConstructionManager bm = GameObject.FindObjectOfType<ConstructionManager>();
+        if (bm.selectedUnit != null)
+        
+        {
+            LifeAmountManager sm = GameObject.FindObjectOfType<LifeAmountManager>();
+            
+
+        
+            Instantiate(bm.selectedUnit, transform.parent.position, transform.parent.rotation);
+            Destroy(transform.parent.gameObject);
+        }
+    }
+
+}
+
+
