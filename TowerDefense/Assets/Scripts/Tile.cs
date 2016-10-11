a4e84d59a14e52689b6b387ee1ab727dbc1cf88d
Pablo <pherresa7@alumnes.ub.edu>
2016-10-11 15:37:48 +0200

    Creation of first version of tile class

---
+++ TowerDefense/Assets/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+// Class to wrap all the data related to a Tile
+public class Tile : MonoBehaviour {
+	public struct m_Coordinates
+	{
+		private int m_x;
+		private int m_y;
+		private int m_z;
+
+	}
+
+	private bool m_IsDestroyable;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
