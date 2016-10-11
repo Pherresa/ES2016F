55a9044df43c42a4dac2d40f60b2277974b6dcb7
Pablo <pherresa7@alumnes.ub.edu>
2016-10-11 16:12:20 +0200

    Creation of class Coordinates.
    Support class to be used in class Tiles to encapsulated coordinates
    infomartion.

---
+++ TowerDefense/Assets/Scripts/Coordinates.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+// Class to store everything related to Coordinates.
+public class Coordinates
+{
+	private int m_pos_X;
+	private int m_pos_Y;
+	private int m_pos_Z;
+	// Use this for initialization
+
+
+	public Coordinates (int x, int y, int z)
+	{
+		m_pos_X = x;
+		m_pos_Y = y;
+		m_pos_Z = z;
+	}
+
+	public int pos_X
+	{
+		get 
+		{ 
+			return m_pos_X; 
+		}
+		
+		set
+		{
+			m_pos_X = value;
+		}
+	}
+
+	public int pos_Y
+	{
+		get 
+		{ 
+			return m_pos_Y; 
+		}
+
+		set
+		{
+			m_pos_Y = value;
+		}
+	}
+
+	public int pos_Z
+	{
+		get 
+		{ 
+			return m_pos_Z; 
+		}
+
+		set
+		{
+			m_pos_Z = value;
+		}
+	}
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
