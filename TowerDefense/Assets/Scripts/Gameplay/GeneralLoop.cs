e65feb596369ec1d3b27bec41a1b21e2f5d135d4
Zamora2 <azamorza7@alumnes.ub.edu>
2016-10-11 17:04:46 +0200

    Modificate function Update

--- TowerDefense/Assets/Scripts/Gameplay/GeneralLoop.cs
+++ TowerDefense/Assets/Scripts/Gameplay/GeneralLoop.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class GeneralLoop : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private bool contin;
+
+    // Use this for initialization
+    void Start () {
+        contin = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+    // Update is called once per frame
+    // Get all the enemies, increases the z position of each of them
+    void Update () {
+        if (contin)
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies) {
+                enemy.transform.position = new Vector3(enemy.transform.position.x, enemy.transform.position.y, enemy.transform.position.z + 1);
+            }
+        }
+    }
 }
