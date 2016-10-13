ff6ef7a4096c973fe61674e945bca4575b69c234
Zamora2 <azamorza7@alumnes.ub.edu>
2016-10-13 16:33:36 +0200

    Modify function Update

--- TowerDefense/Assets/Scripts/Entities/Enemy.cs
+++ TowerDefense/Assets/Scripts/Entities/Enemy.cs
@@ -14,6 +14,12 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Vector3 position_aprox = new Vector3((int)this.transform.position.x, (int)this.transform.position.y, (int)this.transform.position.z);
+        if (position_aprox == m_movi_actu)
+        {
+            m_movi_actu = (Vector3)m_moviments.Dequeue();
+        }
+        transform.LookAt(m_movi_actu);
+        this.transform.Translate(Vector3.forward * m_velocity * Time.deltaTime);
+    }
 }
