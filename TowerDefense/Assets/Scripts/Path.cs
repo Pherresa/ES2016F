bbdd5ef47c0ed7b0e177d99fe95ab0dc8353622d
Pablo <pherresa7@alumnes.ub.edu>
2016-10-14 14:41:23 +0200

    Creation of Path class

--- TowerDefense/Assets/Scripts/Path.cs
+++ TowerDefense/Assets/Scripts/Path.cs
@@ -1,10 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Class to save the data of the path in the map
 public class Path : Tile {
 
-	// Use this for initialization
-	void Start () {
+    private Tile direction;
+    private double m_speed;
+
+    //Constructor
+    public Path (Tile tile, double spd)
+    {
+        direction = tile;
+        m_speed = spd;
+    }
+
+    //Getters and setters
+    public Tile dir
+    {
+        get
+        {
+            return direction;
+        }
+
+        set
+        {
+            direction = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
