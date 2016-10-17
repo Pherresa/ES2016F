6b2840a420394e4e188fd8306a62a87d99b3e163
mpQuintana <marcperez1993@gmail.com>
2016-10-17 16:51:30 +0200

    afegit fons al text de informacio de les unitats i arreglat bugs

--- TowerDefense/Assets/Scripts/UnitInfo.cs
+++ TowerDefense/Assets/Scripts/UnitInfo.cs
@@ -5,11 +5,13 @@ public class UnitInfo : MonoBehaviour
 {
     public GameObject selectedUnit;
     private Text infoUnitText;
+    private CanvasGroup canvasIU;
 
     // Use this for initialization
     void Start()
     {
         infoUnitText = GameObject.Find("infoUnitText").GetComponent<Text>();
+        canvasIU = GameObject.Find("InfoUnit").GetComponent<CanvasGroup>();
     }
 
     // Update is called once per frame
@@ -18,8 +20,15 @@ public class UnitInfo : MonoBehaviour
 
     }
 
+    public void hideInfo()
+    {
+        canvasIU.alpha = 0;
+    }
+
     public void showInfo(GameObject prefab)
     {
+
+        canvasIU.alpha = 1;
         selectedUnit = prefab;
         infoUnitText.text = "Power: " +
             selectedUnit.name.Substring(selectedUnit.name.Length - 3, 3) +
