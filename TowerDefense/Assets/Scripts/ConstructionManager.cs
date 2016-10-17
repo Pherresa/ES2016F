62559a440220c76dd3c0837f5fb55c393a93c401
JuanLuis <jsoriaso7@alumnes.ub.edu>
2016-10-17 19:35:22 +0200

    Available places blinking

--- TowerDefense/Assets/Scripts/ConstructionManager.cs
+++ TowerDefense/Assets/Scripts/ConstructionManager.cs
@@ -4,10 +4,30 @@ using System.Collections;
 public class ConstructionManager : MonoBehaviour
 {
     public GameObject selectedUnit;
+    private PlaceUnit[] placeUnits;
+    private Color[] origColors;
+    private Color colorPlaceUnit;
 
     // Use this for initialization
     void Start()
     {
+        // Get all placeUnits
+        placeUnits = FindObjectsOfType(typeof(PlaceUnit)) as PlaceUnit[]; 
+
+        // In future, get color or material of each placeUnit
+
+        /*int i =0;
+        int n = sizeof(placeUnits) / sizeof(placeUnits[0]);
+        origColors = new Color[n];
+        foreach (var pu in placeUnits){
+            Renderer r = pu.rend;
+            origColors[i] = r.material.color;
+            i = i+1;    
+        }
+        */
+
+        // For now, get color of one placeUnit (suppose all same color)
+        colorPlaceUnit = placeUnits[0].rend.material.color;
 
     }
 
@@ -19,6 +39,35 @@ public class ConstructionManager : MonoBehaviour
 
     public void SelectUnit(GameObject prefab)
     {
+        // For each placeUnit
+        foreach (var pu in placeUnits){
+            // If placeUnit is free
+            if (pu.free == true){
+                // Tell that button is clicked, so free placeUnits can show they are available
+                pu.isClicked(true);
+            }
+        }
+
+
         selectedUnit = prefab;
     }
+
+    public void stopBlinking(){
+        int i = 0;
+        foreach (var pu in placeUnits){
+
+            if (pu.free == true){
+                pu.isClicked(false);
+                Renderer r = pu.rend;
+                // Future
+                //r.material.color = origColors[i];
+                r.material.color = colorPlaceUnit;
+
+            }
+            i=i+1;
+        }
+    }
+
+
+
 }
