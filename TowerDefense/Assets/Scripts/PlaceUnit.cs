ebf956888ccf231d31599d03b4543b30dc4381b4
JuanLuis <jsoriaso7@alumnes.ub.edu>
2016-10-17 11:09:36 +0200

    Show available places

--- TowerDefense/Assets/Scripts/PlaceUnit.cs
+++ TowerDefense/Assets/Scripts/PlaceUnit.cs
@@ -5,6 +5,61 @@ using System;
 
 public class PlaceUnit : MonoBehaviour {
 
+    public float timer;
+    public float WaitTime;
+    public float ResetPoint;
+
+    public Renderer rend;
+    public bool free;
+    public bool clicked;
+
+    public Color origColor;
+
+
+    void Start(){
+        // We get renderer
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        // Get original color
+        origColor = rend.material.color;
+        free = true;
+        clicked = false;
+        WaitTime = 0.5f;
+        ResetPoint = WaitTime * 2;
+
+    }
+
+
+    void Update(){
+        // If one button is clicked, placeUnit blinks
+        if (clicked){
+            timer += Time.deltaTime;
+     
+            if(timer < WaitTime){
+            rend.material.color = origColor;
+            }
+
+            if(timer > WaitTime){
+            rend.material.color = Color.green;
+            }
+
+            if(timer > ResetPoint){
+            timer = 0;
+            }
+        }
+        else{
+            rend.material.color = origColor;
+        }
+    }
+
+    public void isClicked(bool b){
+        clicked = b;
+    }
+
+
+
+
+
 
     void OnMouseUp()
     {
@@ -13,6 +68,10 @@ public class PlaceUnit : MonoBehaviour {
 
         if (bm.selectedUnit != null)
         {
+            free = false;
+            isClicked(false);
+            GameObject.FindObjectOfType<ConstructionManager>().stopBlinking();
+
             LifeAmountManager sm = GameObject.FindObjectOfType<LifeAmountManager>();
 
             GameObject obj = (GameObject) Instantiate(bm.selectedUnit, transform.position + new Vector3(0, 0.5f, 0), transform.rotation);
