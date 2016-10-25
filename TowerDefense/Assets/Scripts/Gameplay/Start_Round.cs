d14cc9028afcf3b207e86c7adffa89ceff8195de
Zamora2 <azamorza7@alumnes.ub.edu>
2016-10-25 19:35:36 +0200

    countdown

---
+++ TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_Round : MonoBehaviour {
+
+    GameObject indicator_time;
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 0.0f;
+        indicator_time = GameObject.FindGameObjectWithTag("Time_Cont");
+        indicator_time.GetComponent<TextMesh>().text = "60";
+    }
+
+    // Update is called once per frame
+    void Update(){
+    }
+
+	void FixedUpdate () {
+        int in_time = int.Parse(indicator_time.GetComponent<TextMesh>().text)-1;
+        indicator_time.GetComponent<TextMesh>().text = in_time.ToString();
+    }
+}
