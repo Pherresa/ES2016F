70844d40c6aa76fa654566f23112ec4de193c96b
Zamora2 <azamorza7@alumnes.ub.edu>
2016-10-25 21:16:27 +0200

    Commentaries

--- TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
+++ TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
@@ -7,23 +7,23 @@ public class Start_Round : MonoBehaviour {
 
     private GameObject indicator_time;
     private float cnt_time;
-    private bool cnt_time_end;
+    private bool cnt_time_end; // When it reaches zero is activated
 
     // Use this for initialization
     void Start () {
         Time.timeScale = 0.0f;
         cnt_time = 0.0f;
         cnt_time_end = false;
-        indicator_time = GameObject.FindGameObjectWithTag("Time_Cont");
+        indicator_time = GameObject.FindGameObjectWithTag("Time_Cont"); // we seek object label
     }
 
     // Update is called once per frame
     void Update() {
-        cnt_time += Time.fixedDeltaTime;
+        cnt_time += Time.fixedDeltaTime; // We count the time between frame
         if (cnt_time >= 1.0f)
         {
             act_time -= 1;
-            if (act_time == 0)
+            if (act_time == 0) // countdown_finish start game
             {
                 GameObject.Destroy(indicator_time);
                 Time.timeScale = 1.0f;
@@ -39,7 +39,7 @@ public class Start_Round : MonoBehaviour {
     }
 
 	public void OnMouseUpAsButton() {
-        if (cnt_time_end) {
+        if (cnt_time_end) { //switch to play or pause
             if (Time.timeScale == 0.0f)
             {
                 Time.timeScale = 1.0f;
@@ -51,8 +51,8 @@ public class Start_Round : MonoBehaviour {
                 this.gameObject.GetComponent<TextMesh>().text = "Play";
             }
         }
-        else
-        {
+        else // If you activate the button before the time runs out the game begins
+        { 
             GameObject.Destroy(indicator_time);
             Time.timeScale = 1.0f;
             cnt_time_end = true;
