0a808d91322eb854b2720b4afeaae81ee880ea28
Zamora2 <azamorza7@alumnes.ub.edu>
2016-10-25 21:11:27 +0200

    switch to continue or pause

--- TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
+++ TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
@@ -7,24 +7,31 @@ public class Start_Round : MonoBehaviour {
 
     private GameObject indicator_time;
     private float cnt_time;
+    private bool cnt_time_end;
 
     // Use this for initialization
     void Start () {
         Time.timeScale = 0.0f;
         cnt_time = 0.0f;
+        cnt_time_end = false;
         indicator_time = GameObject.FindGameObjectWithTag("Time_Cont");
     }
 
     // Update is called once per frame
     void Update() {
         cnt_time += Time.fixedDeltaTime;
-        if (cnt_time >= 1.0f) {
+        if (cnt_time >= 1.0f)
+        {
             act_time -= 1;
             if (act_time == 0)
             {
                 GameObject.Destroy(indicator_time);
                 Time.timeScale = 1.0f;
-            }else {
+                cnt_time_end = true;
+                this.gameObject.GetComponent<TextMesh>().text = "Pause";
+            }
+            else
+            {
                 indicator_time.GetComponent<TextMesh>().text = act_time.ToString();
             }
             cnt_time = 0.0f;
@@ -32,7 +39,24 @@ public class Start_Round : MonoBehaviour {
     }
 
 	public void OnMouseUpAsButton() {
-        GameObject.Destroy(indicator_time);
-        Time.timeScale = 1.0f;
+        if (cnt_time_end) {
+            if (Time.timeScale == 0.0f)
+            {
+                Time.timeScale = 1.0f;
+                this.gameObject.GetComponent<TextMesh>().text = "Pause";
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                this.gameObject.GetComponent<TextMesh>().text = "Play";
+            }
+        }
+        else
+        {
+            GameObject.Destroy(indicator_time);
+            Time.timeScale = 1.0f;
+            cnt_time_end = true;
+            this.gameObject.GetComponent<TextMesh>().text = "Pause";
+        }
     }
 }
