6f36a149ae6b337d744f5de3661a575062ab7481
Zamora2 <azamorza7@alumnes.ub.edu>
2016-10-25 20:16:41 +0200

    Countdown_finish_start_round

--- TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
+++ TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
@@ -4,28 +4,35 @@ using System.Collections;
 public class Start_Round : MonoBehaviour {
 
     public int act_time;
+
     private GameObject indicator_time;
     private float cnt_time;
 
-	// Use this for initialization
-	void Start () {
-        //Time.timeScale = 0.0f;
+    // Use this for initialization
+    void Start () {
+        Time.timeScale = 0.0f;
         cnt_time = 0.0f;
         indicator_time = GameObject.FindGameObjectWithTag("Time_Cont");
-        indicator_time.GetComponent<TextMesh>().text = act_time.ToString();
     }
 
     // Update is called once per frame
-    void Update(){
-    }
-
-	void FixedUpdate () {
-        cnt_time += Time.deltaTime;
-        if (cnt_time >= 1.0f)
-        {
-            int in_time = int.Parse(indicator_time.GetComponent<TextMesh>().text) - 1;
-            indicator_time.GetComponent<TextMesh>().text = in_time.ToString();
+    void Update() {
+        cnt_time += Time.fixedDeltaTime;
+        if (cnt_time >= 1.0f) {
+            act_time -= 1;
+            if (act_time == 0)
+            {
+                GameObject.Destroy(indicator_time);
+                Time.timeScale = 1.0f;
+            }else {
+                indicator_time.GetComponent<TextMesh>().text = act_time.ToString();
+            }
             cnt_time = 0.0f;
         }
+    }
+
+	public void OnMouseUpAsButton() {
+        GameObject.Destroy(indicator_time);
+        Time.timeScale = 1.0f;
     }
 }
