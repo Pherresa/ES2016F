8124373b4b9ea0ddd9d571bbb34546ebc74726c5
Alicia Morales Carrasco <amoralca16@alumnes.ub.edu>
2016-10-26 16:45:45 +0200

    Code review

--- TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
+++ TowerDefense/Assets/Scripts/Gameplay/Start_Round.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Start_Round : MonoBehaviour {
 
     public int act_time;
 
-    private GameObject indicator_time;
+   
     private float cnt_time;
     private bool cnt_time_end; // When it reaches zero is activated
+    public Text indicator_time;
+    public Text buttonText;
 
     // Use this for initialization
     void Start () {
-        Time.timeScale = 0.0f;
+        //Time.timeScale = 0.0f;
         cnt_time = 0.0f;
         cnt_time_end = false;
-        indicator_time = GameObject.FindGameObjectWithTag("Time_Cont"); // we seek object label
+        InvokeRepeating("countDown", 0f, 1f);
     }
 
     // Update is called once per frame
     void Update() {
-        cnt_time += Time.fixedDeltaTime; // We count the time between frame
+        /*cnt_time += Time.fixedDeltaTime; // We count the time between frame
         if (cnt_time >= 1.0f)
         {
             act_time -= 1;
@@ -35,20 +38,20 @@ public class Start_Round : MonoBehaviour {
                 indicator_time.GetComponent<TextMesh>().text = act_time.ToString();
             }
             cnt_time = 0.0f;
-        }
+        }*/
     }
 
-	public void OnMouseUpAsButton() {
+	public void play_or_pause() {
         if (cnt_time_end) { //switch to play or pause
             if (Time.timeScale == 0.0f)
             {
                 Time.timeScale = 1.0f;
-                this.gameObject.GetComponent<TextMesh>().text = "Pause";
+                buttonText.text = "Pause";
             }
             else
             {
                 Time.timeScale = 0.0f;
-                this.gameObject.GetComponent<TextMesh>().text = "Play";
+                buttonText.text = "Play";
             }
         }
         else // If you activate the button before the time runs out the game begins
@@ -56,7 +59,22 @@ public class Start_Round : MonoBehaviour {
             GameObject.Destroy(indicator_time);
             Time.timeScale = 1.0f;
             cnt_time_end = true;
-            this.gameObject.GetComponent<TextMesh>().text = "Pause";
+            buttonText.text = "Pause";
+        }
+    }
+
+    private void countDown() {
+        act_time -= 1;
+        if (act_time == 0) // countdown_finish start game
+        {
+            GameObject.Destroy(indicator_time);
+            Time.timeScale = 1.0f;
+            cnt_time_end = true;
+            buttonText.text = "Pause";
+        }
+        else
+        {
+            indicator_time.text = act_time.ToString();
         }
     }
 }
