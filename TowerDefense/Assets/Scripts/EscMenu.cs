4a9b67275ce09dbb44645c1ba17e5d4dac78f58b
Raul <racu10@gmail.com>
2016-11-03 18:24:27 +0100

    Escape menu added, using prefab and add to camera and set into scrip the menu

---
+++ TowerDefense/Assets/Scripts/EscMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EscMenu : MonoBehaviour {
+
+
+    public void mContinueGame()
+    {   
+        Time.timeScale = 1.0f;
+        GameObject tmp = GameObject.FindGameObjectWithTag("gameExit");
+        tmp.SetActive(false);
+    }
+
+    public void mSaveGame() 
+    {
+
+    }
+
+    public void mExitGame()
+    {
+        print("exit");
+        //mEnablePanel("initMenu");
+        Application.Quit();
+    }
+    public void mReturnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
+
