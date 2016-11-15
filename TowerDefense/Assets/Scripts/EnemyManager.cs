76040c5df779cd8992a55c530e6ccd46124ab531
Pablo <pherresa7@alumnes.ub.edu>
2016-11-15 15:41:18 +0100

    Change enemy to orc and added tag enemy to script

--- TowerDefense/Assets/Scripts/EnemyManager.cs
+++ TowerDefense/Assets/Scripts/EnemyManager.cs
@@ -7,11 +7,14 @@ public class EnemyManager : MonoBehaviour {
 	void Start () {
         for (int i = 0; i < 100; i++)
         {
-            GameObject enemyPrefab = Resources.Load("Prefabs/Enemy") as GameObject;
-            GameObject enemy = Instantiate(enemyPrefab);
-            enemy.transform.parent = transform;
+
+            GameObject enemyPrefab = Resources.Load("Prefabs/attack1_Orc_MT") as GameObject;
+            GameObject enemyOrc = Instantiate(enemyPrefab);
+            enemyOrc.transform.parent = transform;
+            enemyOrc.tag = "Enemy";
+
             //get the thing component on your instantiated object
-            AstarAI astarAI = enemy.GetComponent<AstarAI>();
+            AstarAI astarAI = enemyOrc.GetComponent<AstarAI>();
             astarAI.target = GameObject.FindGameObjectWithTag("Target").transform;
         }
 	}
