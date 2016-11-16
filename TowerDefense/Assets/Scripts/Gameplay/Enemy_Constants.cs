34bfb0d75252a06ff90311891c404ee496996d8f
Zamora2 <azamorza7@alumnes.ub.edu>
2016-11-16 19:20:09 +0100

    Get Unit Values

--- TowerDefense/Assets/Scripts/Gameplay/Enemy_Constants.cs
+++ TowerDefense/Assets/Scripts/Gameplay/Enemy_Constants.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy_Constants : MonoBehaviour {
+public static class Enemy_Constants{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Values
+    public const string LIFE = "l";
+    public const string ATTACK = "a";
+    public const string SPEED = "s";
+    public const string RANGE = "r";
+    public const string MONEY = "m";
+
+    // Enemy
+    public const int LIFE_LITTLE = 100;
+    public const int LIFE_MEDIUM = 200;
+    public const int LIFE_BIG = 400;
+
+    public const int ATTACK_LITTLE = 1;
+    public const int ATTACK_MEDIUM = 2;
+    public const int ATTACK_BIG = 5;
+
+    public const int SPEED_LITTLE = 3;
+    public const int SPEED_MEDIUM = 2;
+    public const int SPEED_BIG = 1;
+
+    public const int MONEY_LITTLE = 25;
+    public const int MONEY_MEDIUM = 50;
+    public const int MONEY_BIG = 100;
+
+
+    // Tower
+    public const int T_RANGE_LITTLE = 500;
+    public const int T_RANGE_MEDIUM = 400;
+    public const int T_RANGE_BIG = 300;
+
+    public const int T_ATTACK_LITTLE = 10;
+    public const int T_ATTACK_MEDIUM = 20;
+    public const int T_ATTACK_BIG = 30;
+
+    // General
+    public const int TIME = 15;
+    public const float TIME_INCREMENT = (float)1.3;
 }
--- TowerDefense/Assets/Scripts/Gameplay/Enemy_Values_Gene.cs
+++ TowerDefense/Assets/Scripts/Gameplay/Enemy_Values_Gene.cs
@@ -1,15 +1,126 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy_Values_Gene : MonoBehaviour {
+public static class Enemy_Values_Gene{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public static int m_little_enemy(string selector) {
+        switch (selector) {
+            case "l":
+                return Enemy_Constants.LIFE_LITTLE;
+                break;
+            case "a":
+                return Enemy_Constants.ATTACK_LITTLE;
+                break;
+            case "s":
+                return Enemy_Constants.SPEED_LITTLE;
+                break;
+            case "m":
+                return Enemy_Constants.MONEY_LITTLE;
+                break;
+            default:
+                Debug.Log("Small enemy does not have this characteristic");
+                break;
+        }
+        return 0;
+    }
+
+    public static int m_medium_enemy(string selector)
+    {
+        switch (selector)
+        {
+            case "l":
+                return Enemy_Constants.LIFE_MEDIUM;
+                break;
+            case "a":
+                return Enemy_Constants.ATTACK_MEDIUM;
+                break;
+            case "s":
+                return Enemy_Constants.SPEED_MEDIUM;
+                break;
+            case "m":
+                return Enemy_Constants.MONEY_MEDIUM;
+                break;
+            default:
+                Debug.Log("Medium enemy does not have this characteristic");
+                break;
+        }
+        return 0;
+    }
+
+    public static int m_big_enemy(string selector)
+    {
+        switch (selector)
+        {
+            case "l":
+                return Enemy_Constants.LIFE_BIG;
+                break;
+            case "a":
+                return Enemy_Constants.ATTACK_BIG;
+                break;
+            case "s":
+                return Enemy_Constants.SPEED_BIG;
+                break;
+            case "m":
+                return Enemy_Constants.MONEY_BIG;
+                break;
+            default:
+                Debug.Log("Big enemy does not have this characteristic");
+                break;
+        }
+        return 0;
+    }
+
+    public static int m_little_tower(string selector)
+    {
+        switch (selector)
+        {
+            case "a":
+                return Enemy_Constants.T_ATTACK_LITTLE;
+                break;
+            case "r":
+                return Enemy_Constants.T_RANGE_LITTLE;
+                break;
+            default:
+                Debug.Log("Little tower does not have this characteristic");
+                break;
+        }
+        return 0;
+    }
+
+    public static int m_medium_tower(string selector)
+    {
+        switch (selector)
+        {
+            case "a":
+                return Enemy_Constants.T_ATTACK_MEDIUM;
+                break;
+            case "r":
+                return Enemy_Constants.T_RANGE_MEDIUM;
+                break;
+            default:
+                Debug.Log("Medium tower does not have this characteristic");
+                break;
+        }
+        return 0;
+    }
+
+
+
+    public static int m_big_tower(string selector)
+    {
+        switch (selector)
+        {
+            case "a":
+                return Enemy_Constants.ATTACK_BIG;
+                break;
+            case "r":
+                return Enemy_Constants.T_RANGE_BIG;
+                break;
+            default:
+                Debug.Log("Big tower does not have this characteristic");
+                break;
+        }
+        return 0;
+    }
 }
