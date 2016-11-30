3fd19ff303fd7e35f85ca9d10b3d87b949b30a0c
Pablo <pherresa7@alumnes.ub.edu>
2016-11-30 17:03:34 +0100

    Added basic data to be saved

---
+++ TowerDefense/Assets/Scripts/Gameplay/Game.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+[System.Serializable]
+public class Game {
+
+    public static Game current;
+    public LifeAmountManager lifeAM;
+    public int savedScore;
+    public int savedTowerLife;
+    public int savedMoney;
+    
+    //TODO: List of updated towers in the scene.
+    
+    
+
+    public Game () {
+        lifeAM = GameObject.FindObjectOfType<LifeAmountManager>();
+        savedScore = lifeAM.calculateFinalScore();
+        savedTowerLife = lifeAM.life;
+        savedMoney = lifeAM.amount;
+    }
+}
